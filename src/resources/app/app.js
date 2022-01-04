c5210fcfa5faa4ddb5f88b38efff9dce13b26add
my4fdal <my4fdal@gmail.com>
2022-01-04 09:50:27 +0700

    after merge overrite

--- src/resources/app/app.js
+++ src/resources/app/app.js
@@ -1,9 +1,8 @@
 import Vue from "vue";
 import {
-    createInertiaApp,
-    plugin as InertiaPlugin,
+  createInertiaApp,
+  plugin as InertiaPlugin,
 } from "@inertiajs/inertia-vue";
-import { Inertia } from "@inertiajs/inertia";
 import { Datetime } from "vue-datetime";
 import moment from "moment";
 import _ from "lodash";
@@ -22,42 +21,57 @@ Vue.mixin({ methods: { route } });
 Vue.use(InertiaPlugin);
 Vue.prototype.$api = api;
 Vue.prototype.$moment = (date, format) => {
-    return moment(date).format(format);
+  return moment(date).format(format);
 };
 Vue.prototype.$constants = {
-    MOBILE: "mobile",
-    DESKTOP: "desktop",
+  MOBILE: "mobile",
+  DESKTOP: "desktop",
 };
 
 Inertia.on("navigate", (event) => {
-    setTimeout(() => {
-        window.titleElement = event.srcElement
-            .getElementById("app")
-            .getElementsByClassName("post-theme__post--title");
+  setTimeout(() => {
+    window.titleElement = event.srcElement
+      .getElementById("app")
+      .getElementsByClassName("post-theme__post--title");
 
-        const page_title = window.titleElement[0]?.innerText ?? document.title;
-        const { href: page_location, pathname: page_path } = window.location;
-        const configuration = {
-            page_title,
-            page_location,
-            page_path,
-        };
+    const page_title = window.titleElement[0]?.innerText ?? document.title;
+    const { href: page_location, pathname: page_path } = window.location;
+    const configuration = {
+      page_title,
+      page_location,
+      page_path,
+    };
 
-        gtag("js", new Date());
-        gtag("config", window.measurement_id, configuration);
-    }, 1000);
+    gtag("js", new Date());
+    gtag("config", window.measurement_id, configuration);
+  }, 1000);
 });
 
 const appName =
-    window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
+  window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => require(`./pages/${name}.vue`),
-    setup({ el, App, props }) {
-        new Vue({
-            store,
-            render: (h) => h(App, props),
-        }).$mount(el);
-    },
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) => {
+    const isExists = require
+      .context(
+        "./../../../../../../resources/js/badaso/theme/post-theme/pages",
+        false,
+        /\.vue$/
+      )
+      .keys()
+      .some((key) => key.includes(name));
+
+    if (isExists) {
+      return require(`./../../../../../../resources/js/badaso/theme/post-theme/pages/${name}.vue`);
+    } else {
+      return require(`./pages/${name}.vue`);
+    }
+  },
+  setup({ el, App, props }) {
+    new Vue({
+      store,
+      render: (h) => h(App, props),
+    }).$mount(el);
+  },
 });
